ab1990bab8c11cd809118c3b3e83d243f60b878b
giacomodeconti <giacomodeconti.gdc@gmail.com>
2022-12-02 20:25:28 +0100

    suka

--- CreatePolicy.py
+++ CreatePolicy.py
@@ -27,55 +27,63 @@ def CreatePolicy():
     if rule=="I":
         os.system(f"echo {PolicyName} {IPs} {port} {protocol} {traffic} >> InBound.txt")
         
-        if IPs=='all' and port!='all' and protocol!="all":
+        if IPs=='all' and port!='all' and protocol!='all':
             #add port and protocol command 
+            print("IPs=='all' and port!='all' and protocol!='all'")
             os.system(f"sudo iptables -I INPUT -p {protocol} --destination-port {port} -j {traffic}")
 
-        elif IPs!='all' and port=='all' and protocol!="all":  
+        elif IPs!='all' and port=='all' and protocol!='all':  
             #add IP and protocol command
+            print("IPs!='all' and port=='all' and protocol!='all'")
             os.system(f"sudo iptables -I INPUT -s {IPs} -p {protocol} -j {traffic}")
 
-        elif IPs!='all' and port!='all' and protocol=="all":
+        elif IPs!='all' and port!='all' and protocol=='all': #error 3
             #add IP and port command
+            print("IPs!='all' and port!='all' and protocol=='all'")
             os.system(f"sudo iptables -I INPUT -s {IPs} --destination-port {port} -j {traffic}")
 
-        elif IPs=="all" and port=='all' and protocol!='all':
+        elif IPs=='all' and port=='all' and protocol!='all':
             #add protocol command
+            print("IPs=='all' and port=='all' and protocol!='all'")
             os.system(f"sudo iptables -I INPUT -p {protocol} -j {traffic}")
 
         elif IPs!='all' and port=='all' and protocol=='all':
             #add IP command
+            print("IPs!='all' and port=='all' and protocol=='all'")
             os.system(f"sudo iptables -I INPUT -s {IPs} -j {traffic}")
 
-        elif IPs=='all' and port!='all' and protocol=='all':
+        elif IPs=='all' and port!='all' and protocol=='all': #error 6
             #add port command
+            print("IPs=='all' and port!='all' and protocol=='all'")
             os.system(f"sudo iptables -I INPUT --destination-port {port} -j {traffic}")
-
+            
         elif IPs=='all' and port=='all' and protocol=='all':
             #all parameters
+            print("IPs=='all' and port=='all' and protocol=='all'")
             os.system(f"sudo iptables -I INPUT -j {traffic}")
 
         elif IPs!='all' and port!='all' and protocol!='all':
             #one by one
+            print("IPs!='all' and port!='all' and protocol!='all'")
             os.system(f"sudo iptables -I INPUT -s {IPs} -p {protocol} --destination-port {port} -j {traffic}")
             
     # OUTPUT RULE
     elif rule=="O":
         os.system(f"echo {PolicyName} {IPs} {port} {protocol} {traffic} >> OutBound.txt")
 
-        if IPs=='all' and port!='all' and protocol!="all":
+        if IPs=='all' and port!='all' and protocol!='all':
             #add port and protocol command 
             os.system(f"sudo iptables -I INPUT -p {protocol} --destination-port {port} -j {traffic}")
 
-        elif IPs!='all' and port=='all' and protocol!="all":  
+        elif IPs!='all' and port=='all' and protocol!='all':  
             #add IP and protocol command
             os.system(f"sudo iptables -I INPUT -s {IPs} -p {protocol} -j {traffic}")
 
-        elif IPs!='all' and port!='all' and protocol=="all":
+        elif IPs!='all' and port!='all' and protocol=='all':
             #add IP and port command
             os.system(f"sudo iptables -I INPUT -s {IPs} --destination-port {port} -j {traffic}")
 
-        elif IPs=="all" and port=='all' and protocol!='all':
+        elif IPs=='all' and port=='all' and protocol!='all':
             #add protocol command
             os.system(f"sudo iptables -I INPUT -p {protocol} -j {traffic}")
 
