a83e4aae1c82a18c44da330113e779406f931cb3
ArmandoBattaglino <armando.battaglino1@gmail.com>
2022-12-03 19:54:14 +0100

    some changes in delPolicy

--- delPolicy.py
+++ delPolicy.py
@@ -9,56 +9,80 @@ def delPolicy():
 
       # file finder by input
       if respond == "I":
-         file_chosen = "InBound.txt"
+         chosen_file = "InBound.txt"
 
       elif respond == "O":
-         file_chosen = "OutBound.txt"
+         chosen_file = "OutBound.txt"
 
-      # call nameCheck function
-      occurrencies = nameCheck(string_input,file_chosen)
+      # Looks for occurrencies of the rule name in the file and stores them in a list
+      occurrencies = nameCheck(string_input,chosen_file)
 
+      #TO DO: Insert a control that checks if there are similar names or if theres is none similar to the input
       print("There are files that have the same name, which one do you want to delete?")
+
+      #Displays occurrencies of the rule name with index to let the user choose the right one
       for idx,name in enumerate(occurrencies):
          print(f" {idx + 1} >>> {name}")
-      
+
+      # Asking user for the rule to be deleted
       del_rule = int(input("Which rule do you want to delete?"))
-      
-      with open(file_chosen, "r") as inputd:
+      to_be_del = occurrencies[del_rule-1]
+
+      # Fetch the file and Tranforms all the properties of the rule that has to be deleted in a list
+      del_rule_props = seekRule(to_be_del, chosen_file)
+
+      #Defining all rule properties from del_rule_props (will be used in )
+      r_prop = {
+        "policyName": del_rule_props[0],
+        "IPs": del_rule_props[1],
+        "IPd": del_rule_props[2],
+        "port": del_rule_props[3],
+        "protocol": del_rule_props[4],
+        "traffic": del_rule_props[5]
+      }
+
+
+      with open(chosen_file, "r") as inputd:
          with open("temp.txt", "w") as output:
-            # iterate all lines from file
+            # reading all lines of the file
             for line in inputd:
                rule = line.split(" ")
-               if occurrencies[del_rule-1] != rule[0]:
+               #Looks in the file for the rule name of the rule that has to be deleted
+               cline_rule_name = rule[0]
+               if cline_rule_name == to_be_del:
+                  #If the rule name of the current line (cline_rule_name) is the same of the the rule that has to be deleted (to_be_del),
+                  write_here_function_that_deletes = None
+               elif cline_rule_name != to_be_del:
+                  #If the rule name of the current line is different from the rule name of the rule that has to be deleted, it writes it on the temporary file that will then be swapped with the current file
                   output.write(line)
 
-      rule_p = seekRule(occurrencies[del_rule-1], file_chosen)
 
       '''
       if respond == "I":
-         os.system(f"sudo iptables -D INPUT -s {rule_p[1]} -j {rule_p[4]}")
+         os.system(f"sudo iptables -D INPUT -s {del_rule_props[1]} -j {del_rule_props[4]}")
       elif respond == "O":
-         os.system(f"sudo iptables -D OUTPUT -s {rule_p[1]} -j {rule_p[4]}")'''
+         os.system(f"sudo iptables -D OUTPUT -s {del_rule_props[1]} -j {del_rule_props[4]}")'''
 
       if respond == "I":
     #icmp requests
-         if rule_p[4]=='icmp':
-               os.system(f"sudo iptables -D INPUT -s {rule_p[1]} -d {rule_p[2]} -p {rule_p[4]} -j {rule_p[5]} --icmp-type echo-request")
-         elif rule_p[4] == 'all':
-               os.system(f"sudo iptables -D INPUT -s {rule_p[1]} -d {rule_p[2]} -p tcp --dport {rule_p[3]} -j {rule_p[5]}")
-               os.system(f"sudo iptables -D INPUT -s {rule_p[1]} -d {rule_p[2]} -p udp --dport {rule_p[3]} -j {rule_p[5]}")
+         if del_rule_props[4]=='icmp':
+               os.system(f"sudo iptables -D INPUT -s {del_rule_props[1]} -d {del_rule_props[2]} -p {del_rule_props[4]} -j {del_rule_props[5]} --icmp-type echo-request")
+         elif del_rule_props[4] == 'all':
+               os.system(f"sudo iptables -D INPUT -s {del_rule_props[1]} -d {del_rule_props[2]} -p tcp --dport {del_rule_props[3]} -j {del_rule_props[5]}")
+               os.system(f"sudo iptables -D INPUT -s {del_rule_props[1]} -d {del_rule_props[2]} -p udp --dport {del_rule_props[3]} -j {del_rule_props[5]}")
          else:
-               os.system(f"sudo iptables -D INPUT -s {rule_p[1]} -d {rule_p[2]} -p {rule_p[4]} --dport {rule_p[3]} -j {rule_p[5]}")
+               os.system(f"sudo iptables -D INPUT -s {del_rule_props[1]} -d {del_rule_props[2]} -p {del_rule_props[4]} --dport {del_rule_props[3]} -j {del_rule_props[5]}")
 
       elif respond == "O":
-         if rule_p[4] == 'icmp':
-               os.system(f"sudo iptables -D OUTPUT -s {rule_p[1]} -d {rule_p[2]} -p {rule_p[4]} -j {rule_p[5]} --icmp-type echo-reply")
-         elif rule_p[4] == 'all':
-               os.system(f"sudo iptables -D OUTPUT -s {rule_p[1]} -d {rule_p[2]} -p tcp --dport {rule_p[3]} -j {rule_p[5]}")
-               os.system(f"sudo iptables -D OUTPUT -s {rule_p[1]} -d {rule_p[2]} -p udp --dport {rule_p[3]} -j {rule_p[5]}")
+         if del_rule_props[4] == 'icmp':
+               os.system(f"sudo iptables -D OUTPUT -s {del_rule_props[1]} -d {del_rule_props[2]} -p {del_rule_props[4]} -j {del_rule_props[5]} --icmp-type echo-reply")
+         elif del_rule_props[4] == 'all':
+               os.system(f"sudo iptables -D OUTPUT -s {del_rule_props[1]} -d {del_rule_props[2]} -p tcp --dport {del_rule_props[3]} -j {del_rule_props[5]}")
+               os.system(f"sudo iptables -D OUTPUT -s {del_rule_props[1]} -d {del_rule_props[2]} -p udp --dport {del_rule_props[3]} -j {del_rule_props[5]}")
          else:
-               os.system(f"sudo iptables -D OUTPUT -s {rule_p[1]} -d {rule_p[2]} -p {rule_p[4]} --dport {rule_p[3]} -j {rule_p[5]}")
-      
+               os.system(f"sudo iptables -D OUTPUT -s {del_rule_props[1]} -d {del_rule_props[2]} -p {del_rule_props[4]} --dport {del_rule_props[3]} -j {del_rule_props[5]}")
+
       print('POLICY Deleted')
 
       # replace file with original name
-      os.replace('temp.txt', file_chosen)
+      os.replace('temp.txt', chosen_file)
